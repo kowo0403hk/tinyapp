fd759c6e1667a22cd7d405f92e5966f45b2ca07d
Alex Cheung <alexc.sfu@gmail.com>
2022-05-17 01:31:24 +0000

    addded post method to server and urls_new.ejs

--- express_server.js
+++ express_server.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const morgan = require('morgan'); // morgan is useful to log records on the server terminal
+const bodyParser = require("body-parser");
+
 const app = express();
 const PORT = 8080;
 
 // use ejs template
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabse = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 }
 
+// use of middleware
 app.use(morgan('dev'));
-
-// app.use((req, res, next) => { //middleware to parse, we can use the coolie-parser, or the body-parser, or whatever you want
-//   console.log(res);
-//   next();
-// })
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -27,12 +27,15 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabse[req.params.shortURL] };
   res.render('urls_show', templateVars);
 })
-
-
 
 app.get('/hello', (req, res) => {
   res.send('<html><body><b>Hello!</b></body></html>');
@@ -41,6 +44,12 @@ app.get('/hello', (req, res) => {
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabse);
 })
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
 
 // variable experiments
 app.get('/set', (req, res) => {
