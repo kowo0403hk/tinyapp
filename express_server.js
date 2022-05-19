3c7ba9bf4cc0d91b7cf8a7d78b336f160abf2478
Alex Cheung <alexc.sfu@gmail.com>
2022-05-19 01:09:10 +0000

    added login and register functions, changed cookie id to user_id instead of using username

--- express_server.js
+++ express_server.js
@@ -6,7 +6,6 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080;
 
-// use ejs template
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
@@ -26,6 +25,11 @@ const users = {
     id: "user2RandomID", 
     email: "user2@example.com", 
     password: "dishwasher-funk"
+  },
+  "test": {
+    id: "test", 
+    email: "test@test.com", 
+    password: "1234"
   }
 }
 
@@ -36,26 +40,37 @@ app.use(morgan('dev'));
 // GET method
 ////////////////////////////////////////////////////////////////
 
+//login page
+app.get('/login', (req, res) => {
+  const templateVars = {
+    user: users[req.cookies["user_id"]]
+  };
+  res.render('login', templateVars);
+});
+
+//home page
 app.get('/urls', (req, res) => {
+  console.log(urlDatabase, '\n', users);
   const templateVars = { 
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user: users[req.cookies["user_id"]]
   };
-  console.log(templateVars);
-  console.log(req.cookies.username);
   res.render('urls_index', templateVars);
 });
 
+//create new URL page
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"]
+    user: users[req.cookies["user_id"]]
   };
   res.render("urls_new", templateVars);
 });
 
+// register page
 app.get('/register', (req, res) => {
   const templateVars = { 
-    username: req.cookies["username"] };
+    user: users[req.cookies["user_id"]]
+  };
   res.render('register', templateVars);
 })
 
@@ -65,7 +80,7 @@ app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { 
     shortURL: req.params.shortURL, 
     longURL: urlDatabase[req.params.shortURL],
-    username: req.cookies["username"] };
+    user: users[req.cookies["user_id"]] };
   res.render('urls_show', templateVars);
 })
 
@@ -87,23 +102,46 @@ app.get('/u/:shortURL', (req, res) => {
 
 // setting up cookie
 app.post('/login', (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect('/urls');
+  for (const user in users) {
+    if (emailLookUp(req.body, users) && users[user]["password"] === req.body.password) {
+      res.cookie("user_id", user);
+      return res.redirect('/urls');
+    }
+  }
+
+  res.statusCode = 403;
+  res.send(res.statusCode);
 })
 
 app.post('/logout', (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect('/urls');
 })
 
+
+
 app.post('/register', (req, res) => {
-  res.redirect('/urls');
+  if (emailLookUp(req.body, users)) {
+    res.statusCode = 400;
+    res.clearCookie("user_id");
+    return res.sendStatus(res.statusCode);
+  } else {
+    let generatedID = randomID();
+    users[generatedID] = {
+      id: generatedID,
+      email: req.body.email,
+      password: req.body.password
+    }
+    res.cookie('user_id', generatedID);
+    res.redirect('/urls');
+  }
 })
 
 // for new short URL creation
 app.post('/urls', (req, res) => {
-  // after receiveing the longURL input from the user, run the redirect function
-  res.redirect(`/urls/${randomString(req.body)}`);
+  let shortURL = randomID();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 
@@ -123,20 +161,35 @@ app.post('/urls/:id', (req, res) => {
 
 
 
+////////////////////////////////////////////////////////////////
+// Error catcher and misc.
+////////////////////////////////////////////////////////////////
 
-// a catch all app.get and it has to be at the end because it will catch all the route user enters, the statusCode will always be 200, instead of 404, unless you also put in res.statusCode = 404 or res.statusCode(404) alone inside this app.get callback
+
 app.get('*', (req, res) => {
-  // res.send(`This is not the page you are looking for`); ==> this will result in a 200 status code
   res.statusCode = 404;
-  return res.send('Page not found');
+  return res.sendStatus(res.statusCode);
 })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
 
-function randomString(obj) {
-  let shortURL = Math.floor((1 + Math.random()) * 0x100000).toString(16);
-  urlDatabase[shortURL] = obj.longURL;
-  return shortURL;
+
+////////////////////////////////////////////////////////////////
+// Functions to be exported
+////////////////////////////////////////////////////////////////
+
+function randomID() {
+  return Math.floor((1 + Math.random()) * 0x100000).toString(16);
 }
+
+function emailLookUp(reqBody, DataBase) {
+  for (let id in DataBase) {
+    if (DataBase[id]["email"] === reqBody["email"]) {
+      return true;
+    }
+  }
+  return false;
+}
+
